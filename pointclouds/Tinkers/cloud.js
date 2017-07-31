0db8271241f77f1732cf32ff6615a8f75d2eb048
Dakota Benjamin <dakota.benjamin@gmail.com>
2017-07-31 10:40:22 -0400

    MAke potree website

---
+++ pointclouds/Tinkers/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3639952,
+    "boundingBox": {
+        "lx": 2222699.848,
+        "ly": 623516.999,
+        "lz": 645.821,
+        "ux": 2222815.322,
+        "uy": 623632.4729999999,
+        "uz": 761.2949999999292
+    },
+    "tightBoundingBox": {
+        "lx": 2222699.848,
+        "ly": 623516.999,
+        "lz": 645.821,
+        "ux": 2222815.322,
+        "uy": 623619.147,
+        "uz": 716.05
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.000034213066101,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
